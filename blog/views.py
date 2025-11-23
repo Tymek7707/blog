cbf2b7ed2c4ec075b1402204bda60b648a5f1a44
tymek7707 <trymek7707@gmail.com>
2025-11-23 20:13:37 +0100

    added models instead of static data

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from datetime import date
+from .models import Post
 all_posts = [{
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
@@ -64,21 +65,20 @@ all_posts = [{
         """
     }
 ]
-def get_date(post):
-    return post['date']
+
 # Create your views here.
 def starting_page(request):
-    sorted_posts =sorted(all_posts, key = get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, 'blog/index.html',{
         'posts' : latest_posts
     })
 def posts(request):
+    all_posts = Post.objects.all()
     return render(request, 'blog/all-posts.html',{
         'all_posts': all_posts
     })
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post-detail.html', {
         'post': identified_post
     })
